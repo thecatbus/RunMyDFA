96f9aef1203b2ece8a1741cff9e9b1520a3b9e52
Parth Shimpi <pas201@cam.ac.uk>
2021-01-23 16:47:06 +0530

    Update finite_automaton.js

--- finite_automaton.js
+++ finite_automaton.js
@@ -1,20 +1,49 @@
-class State {
-    constructor(position=(0, 0), label="", transitions=[]){
-        this.position = position;
-        this.label = label;
-        this.transitions = transitions;
-    }
+class State { 
+	constructor(name=0, 
+		    position=(0, 0), 
+		    relative=(0, "left"), 
+		    accepting=false, 
+		    initial=false,
+		    label="", 
+		    transitions=[]){ 
+		this.name = name;
+		this.position = position; 
+		this.relative = relative;
+		this.accepting = accepting;
+		this.initial = initial;
+		this.label = label; 
+		this.transitions = transitions; 
+	} 
 }
 
-class Finite_Automaton {
-    constructor(alphabet=["0", "1"], states=[]){
-        this.alphabet = alphabet;
-        this.states = states;
-    }
+class Transition {
+	constructor(from=0,
+		    to=0, 
+		    args=[],
+		    bend="",
+		    label=""){
+		this.from = from;
+		this.to = to;
+		this.args = args;
+		this.bend = bend;
+		this.label = label;
+	}
+}
 
-    addNode(node) {
-        this.states.append(node);
-    }
+class Finite_Automaton { 
+	constructor(alphabet=["0", "1"], states=[], transitions=[]){ 
+		this.alphabet = alphabet; 
+		this.states = states; 
+		this.transitions = transitions; 
+	} 
+
+	addNode(node) { 
+		this.states.append(node); 
+	} 
+
+	addArrow(transition) {
+		this.transitions.append(transition)
+	}
 }
 
 class DFA extends Finite_Automaton {
